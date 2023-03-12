b71af45a88bb58853b22ab06ec0eeccc8893eeda
Marlon E Santos <marlonemanuelgm@gmail.com>
2023-03-11 23:51:47 -0300

    primeiro commit

---
+++ ambiente.js
@@ -0,0 +1,23 @@
+/*var c = 1
+do{
+    console.log (`passo ${c}`)
+    c++
+
+}while (c <= 3) */
+
+
+
+//a mais encontrada nos programas
+
+var c = 1 
+while (c <= 6 ){
+    console.log(`passo ${c}`)
+    c++
+}
+
+/* 
+pior solução
+console.log ('tudo bem?')
+console.log ('tudo bem?')
+console.log ('tudo bem?')
+*/
---
+++ ex013.js
@@ -0,0 +1,45 @@
+/*var agora = new Date()
+var diasem = agora.getDay()
+diasem = */
+/*
+0-Dom
+1-Seg
+2-Ter
+3-Qua
+4-Qui
+5-Sex
+6-Sab*/
+/*console.log(diasem)
+switch (diasem){
+    case 0:
+        console.log('Dom')
+        break
+    case 1:
+        console.log ('Seg')
+        break
+    case 2:
+        console.log ('Ter')
+        break
+    case 3: 
+        console.log ('Qua')
+        break
+    case  4: 
+        console.log ('Qui')
+        break
+    case 5 : 
+        console.log ('Sex')
+    break
+    case 6: 
+       console.log ('Sab')
+       break
+       default :
+       console.log('[ERRO] Dia  inválido')
+       break
+
+}*/
+
+console.log ('vai começar...')
+for (var c=1; c <= 5;c++) {
+    console.log(c)
+}
+console.log('FIM')
